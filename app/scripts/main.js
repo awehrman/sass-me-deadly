1bad37f578bafe20ab26264a856774f2748d0b8d
Alexandra Wehrman <wehrmana@STLC02ND0W8G3QP.local>
2017-05-23 16:25:41 -0500

    Initial animations

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+var credits = document.createElement("div");
+credits.className = "crawl";
+credits.innerHTML = "Deadly\"<br/> \"Kiss Me<br/> <br/><br/> Spillane\'s<br/> Mickey<br/> <span class=\"lower\">Presents</span><br/> Saville<br/> Victor";
+
+var element = document.getElementById("deadly");
+element.appendChild(credits);
