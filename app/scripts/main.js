5f197e8da1e5b8d7f160548e33848d9263177077
Alexandra Wehrman <wehrmana@STLC02ND0W8G3QP.local>
2017-05-24 09:12:06 -0500

    Initial animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,16 @@
-var credits = document.createElement("div");
-credits.className = "crawl";
-credits.innerHTML = "Deadly\"<br/> \"Kiss Me<br/> <br/><br/> Spillane\'s<br/> Mickey<br/> <span class=\"lower\">Presents</span><br/> Saville<br/> Victor";
+let data = "Victor Saville *Presents* Mickey Spillane\'s \n\n \"Kiss&nbsp;Me Deadly \n\n";
+data = data.split(" ").reverse();
 
-var element = document.getElementById("deadly");
+data = data.map(word => {
+	if (~word.indexOf('*')) {
+		return "<br/><span class=\"lower\">" + word.split('*')[1] + "</span><br/>";
+	}
+	return word + "<br />";
+})
+
+const credits = document.createElement("div");
+credits.className = "crawl";
+credits.innerHTML = data.join('');
+
+const element = document.getElementById("deadly");
 element.appendChild(credits);
